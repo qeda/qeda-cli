6aa9db95cda613a51b9d632465f162e966b77774
Shamil Yakupov <s.yakupov@noviga.com>
2019-05-29 20:30:01 +0300

    Minor refactoring

--- src/patterns/mod.rs
+++ src/patterns/mod.rs
@@ -14,7 +14,7 @@ pub trait PatternHandler {
 }
 
 impl Debug for PatternHandler {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "PatternHandler")
     }
 }
--- src/symbols/mod.rs
+++ src/symbols/mod.rs
@@ -14,7 +14,7 @@ pub trait SymbolHandler {
 }
 
 impl Debug for SymbolHandler {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "SymbolHandler")
     }
 }
