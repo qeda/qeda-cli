0cc1b9b674e02b53cb4a2849938ce4fec0669ff5
Shamil Yakupov <s.yakupov@noviga.com>
2019-06-04 11:51:02 +0300

    Optimize SVG a little bit more

--- scripts/optimize-svg.sh
+++ scripts/optimize-svg.sh
@@ -6,4 +6,4 @@
 set -e
 cd "$(dirname "$(readlink -f "${0}")")/../src"
 
-find . -name "*.svg" -exec sh -c  "echo {}; svgcleaner '{}' '{}' --simplify-transforms --properties-precision 2" \;
+find . -name "*.svg" -exec sh -c  "echo {}; svgcleaner '{}' '{}' --simplify-transforms --paths-coordinates-precision 2 --properties-precision 2" \;
